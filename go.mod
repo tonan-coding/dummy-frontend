f8a39456e35961e6c6d95590ff74cad7612ffeb2
Ming Qian <tonan.coding@gmail.com>
2025-07-20 12:39:22 -0400

    setup dummy frontend

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module dummy-frontend
 
 go 1.24
+
+require github.com/urfave/cli/v3 v3.3.8
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/urfave/cli/v3"
+	"net/http"
+)
 
+func main() {
+	cmd := &cli.Command{
+		Name:  "dummy-frontend",
+		Usage: "Dummy frontend implementation",
+		Flags: []cli.Flag{},
+		Action: func(ctx context.Context, command *cli.Command) error {
+			r := mux.NewRouter()
+			r.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("home_page"))
+			})
+			http.ListenAndServe(":8080", r)
+			return nil
+		},
+	}
+	cmd.Run(context.Background(), nil)
 }
